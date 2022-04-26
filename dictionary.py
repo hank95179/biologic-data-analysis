8cf8e96cfc357f90d6905020544d16ca54a984ff
hank95179 <hank95179@gmail.com>
2022-04-26 11:40:59 +0800

    0426hw

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,5 @@
 import re
+from collections import defaultdict
 def Unique(n):
     ans = []
     for i in n:
@@ -19,7 +20,7 @@ def WordCount(f):
     line = fin.readline()
     while line:
         for iterator in mark_set:
-                line= line.replace(iterator,'')
+                line = line.replace(iterator,'')
                 line = line.replace('\n','') #replace space
                 line = line.lower()
         str = line.rstrip()
@@ -35,8 +36,11 @@ def WordCount(f):
     #     if e == 'of':
     #         print("Y")
     sorted_count = sorted(ec.items(),key = lambda d:d[1],reverse = True)
-    print(sorted_count)
-    return sorted_count
+    data_dict = {}
+    for x in sorted_count:
+        data_dict.setdefault(x[0],[]).append(x[1])
+    print(data_dict)
+    return data_dict
 #####
 eng2sp = {'one':'uno','two':'dps'}
 # for eng in eng2sp:
@@ -60,9 +64,39 @@ def Histogram(n):
     return ec
 # ShowDict(LoadDict("dict.txt"))
 #####
+def  MicrornaTarget(fi, fo):
+    d = {}
+    fin = open(fi,'r')
+    fout = open(fo,'w')
+    line = fin.readline()
+    line = fin.readline()
+    answer = defaultdict(list)
+    # for i in range(58510):
+    #     line = fin.readline()
+    while line:
+    # for i in range(58510,58520):
+        a = line.rstrip()
+        str = re.split(' |//|\t',a)
+        # print(str)
+        for i in str:
+            if i == 'Luciferase':
+                if str[4] not in answer[str[1]]:
+                    answer[str[1]].append(str[4])
+                # print(answer)
+        line = fin.readline()
+    for k,v in answer.items():
+        fout.write(k+"  ")
+        for i in range(len(v)):
+            if i == 0:
+                fout.write(v[i])
+            else: 
+                fout.write(","+v[i])
+        fout.write("\n")
+    
 a = [2,7,5,7,5,7,5]
 ec = Histogram(a)
 # ShowDict(ec)
-ua = Unique(a)
+# ua = Unique(a)
 WordCount("nature_short.txt")
+MicrornaTarget('hsa_MTI.txt', 'output.txt')
 # print(ua)
